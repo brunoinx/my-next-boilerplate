6e7d28f494ffaa6de58bc28d22ee5e13963030b7
Bruno Sousa <brunoh.henriqueh01@gmail.com>
2022-10-25 11:36:59 -0300

    feat: create first component Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,20 @@
-const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
-);
+import * as S from './styles';
 
-export default Main;
+export const Main = () => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React avançado escrito ao lado"
+    />
+
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      React, Typescript, NextJS and Styled Components
+    </S.Description>
+
+    <S.Ilustration
+      src="/img/hero-illustration.svg"
+      alt="Programador de frente para uma tela com código"
+    />
+  </S.Wrapper>
+);
